00434a8a7fe4afb5353c37ee0b33222492ce71fc
DHTMLGoodies <post@dhtmlgoodies.com>
2013-02-25 16:55:10 +0100

    Added files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LudoDB - PHP Framework</title>
+    <link rel="stylesheet" href="css/index.css" type="text/css" />
+</head>
+<body>
+<div class="content">
+    <div class="heading">
+        <div class="logo">
+
+        </div>
+    </div>
+    <div class="body">
+        <p>LudoDB is a PHP framework providing an easy way to work with PHP database model objects and offer them as
+        web services to a view.</p>
+        <p>LudoDB with samples can be downloaded from:</p>
+        <div class="code-line">
+            <a href="https://github.com/DHTMLGoodies/ludoDB">https://github.com/DHTMLGoodies/ludoDB</a>
+        </div>
+        <p>Install from command line:</p>
+        <div class="code-line">
+        git clone https://github.com/DHTMLGoodies/ludoDB.git
+        </div>
+        <p>For documentation, see readme files at :</p>
+        <div class="code">
+        <a href="https://github.com/DHTMLGoodies/ludoDB">https://github.com/DHTMLGoodies/ludoDB</a><br>
+        <a href="https://github.com/DHTMLGoodies/ludoDB/tree/master/examples/cities">https://github.com/DHTMLGoodies/ludoDB/tree/master/examples/cities</a><br>
+        <a href="https://github.com/DHTMLGoodies/ludoDB/tree/master/examples/mod_rewrite">https://github.com/DHTMLGoodies/ludoDB/tree/master/examples/mod_rewrite</a><br>
+        </div>
+    </div>
+    <div class="footer">
+        &copy; 2012-<?php echo date("Y"); ?>, Alf Magne Kalleland, LudoDB.com
+    </div>
+</div>
+</body>
+</html>
