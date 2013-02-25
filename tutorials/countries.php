c7f42338bfcf4a0ad8328d83f2df0a5e7963d412
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-25 23:50:55 +0100

    message in countries.php

--- tutorials/countries.php
+++ tutorials/countries.php
@@ -1,7 +1,7 @@
-<p>Example of LudoDBCollection Countrires rendered inside a LudoJS tree.Tree class.</p>
 <?php
 require_once("../includes/header.php");
 ?>
+<p>Example of LudoDBCollection Countrires rendered inside a LudoJS tree.Tree class.</p>
 <script type="text/javascript" src="../ludojs/mootools/mootools-core-1.4.5.js"></script>
 <script type="text/javascript" src="../ludojs/mootools/mootools-more-1.4.0.1.js"></script>
 <script type="text/javascript" src="../ludojs/js/ludojs.js"></script>
